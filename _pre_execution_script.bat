bc701634a395e635323c252c09a7015b53218795
lewk-io <76540899+lewk-io@users.noreply.github.com>
2021-07-19 14:53:10 -0700

    Finish start server script

--- _pre_execution_script.bat
+++ _pre_execution_script.bat
@@ -1,8 +1,1 @@
-cd "BEC"
-call start "BEC - Test server" Bec.exe -f Config.cfg --dsc & ^
-cd "..\profiles\Trader"
-call git pull & ^
-call npm run build-chernarus
-cd "..\PlayerLoadouts"
-call git pull & ^
-call npm start
+node _start-server
--- _start-server.js
+++ _start-server.js
@@ -1,12 +1,57 @@
+const fs = require('fs');
 const path = require('path');
-const { exec, spawn } = require('child_process');
+const { exec, execSync, spawn } = require('child_process');
 
-const getPath = p => path.resolve(p)
+const getPath = p => p ? path.resolve(__dirname, p) : __dirname;
 
-const BEC = spawn('BEC.exe', ['-f Config.cfg', '--dsc'], { cwd: getPath('BEC'), shell: true, detached: true });
-const TRADER = spawn('git pull && npm install && npm run build-chernarus', [], { cwd: getPath('profiles/Trader'), shell: true, detached: true });
+const getMap = /template="(.*)";/gm.exec(fs.readFileSync( getPath('serverDZ.cfg'), { encoding:'utf8' }))[1];
 
-exec('npm install', { cwd: getPath('profiles/Trader'), shell: true, detached: true }, (err, stdout, stderr) => {
+let mapName;
+switch (true) {
+    case getMap.includes('chernarus'):
+        mapName = 'chernarus';
+    case getMap.includes('deerisle'):
+        mapName = 'deerisle';
+    case getMap.includes('esseker'):
+        mapName = 'esseker';
+    case getMap.includes('rostow'):
+        mapName = 'rostow';
+    case getMap.includes('takistan'):
+        mapName = 'takistan';
+    default:
+        mapName = 'chernarus';
+}
+
+console.log(`###### Building for map "${mapName}"`);
+
+// Pull updates from heros-haven-dayz-server repo
+execSync('git pull', { cwd: getPath() }, (err, stdout, stderr) => {
     if (err || stderr) console.log(err || stderr);
     console.log(stdout);
-})
+    console.log('###### Server repo pulled');
+
+    // Sync submodules
+    execSync('git submodule update --init --recursive', { cwd: getPath() }, (err, stdout, stderr) => {
+        if (err || stderr) console.log(err || stderr);
+        console.log(stdout);
+        console.log('###### Submodules synced');
+    })
+});
+
+// Start BEC
+const BEC = spawn('BEC.exe', ['-f Config.cfg', '--dsc'], { cwd: getPath('BEC'), shell: true, detached: true });
+console.log('###### Starting BEC');
+
+// Install PlayerLoadouts dependancies and build
+execSync('npm start', { cwd: getPath('profiles/PlayerLoadouts'), shell: true, detached: true }, (err, stdout, stderr) => {
+    if (err || stderr) console.log(err || stderr);
+    console.log(stdout);
+    console.log('###### PlayerLoadouts repo installed and built');
+});
+
+// Install Trader dependancies and build
+execSync(`npm install && npm run build-${mapName}`, { cwd: getPath('profiles/Trader'), shell: true, detached: true }, (err, stdout, stderr) => {
+    if (err || stderr) console.log(err || stderr);
+    console.log(stdout);
+    console.log('###### Trader repo installed and built');
+});
