a7684418e90f28483bd2aa60cd09bd66661e0c4f
lewk-io <76540899+lewk-io@users.noreply.github.com>
2021-07-19 04:37:34 -0700

    Update gitignore and server start script

--- _start-server.js
+++ _start-server.js
@@ -1,14 +1,12 @@
 const path = require('path');
-const { spawn } = require('child_process');
+const { exec, spawn } = require('child_process');
 
-const BEC = spawn('BEC.exe', ['-f Config.cfg', '--dsc'], { cwd: path.resolve('BEC'),shell: true, detached: true });
+const getPath = p => path.resolve(p)
 
-BEC.stdout.on('data', (data) => {
-console.log(`stdout: ${data}`);
-});
-BEC.on('errorr', (code) => {
-console.log(`child process errored with code ${code}`);
-});
-BEC.on('close', (code) => {
-console.log(`child process exited with code ${code}`);
-});
+const BEC = spawn('BEC.exe', ['-f Config.cfg', '--dsc'], { cwd: getPath('BEC'), shell: true, detached: true });
+const TRADER = spawn('git pull && npm install && npm run build-chernarus', [], { cwd: getPath('profiles/Trader'), shell: true, detached: true });
+
+exec('npm install', { cwd: getPath('profiles/Trader'), shell: true, detached: true }, (err, stdout, stderr) => {
+    if (err || stderr) console.log(err || stderr);
+    console.log(stdout);
+})
