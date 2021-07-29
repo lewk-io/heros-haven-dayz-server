f5500e0ee4305bd1e9d17693e8ed51b77a1f28bf
lewk-io <76540899+lewk-io@users.noreply.github.com>
2021-07-29 07:13:58 -0700

    Copy maps from _MAPS folder

--- _start-server.js
+++ _start-server.js
@@ -1,6 +1,15 @@
 const fs = require('fs');
+const fse = require('fs-extra');
 const path = require('path');
+const { copyFile } = require('path');
 const { exec, execSync, spawn } = require('child_process');
+
+const enableSyncThis = false;
+const enableStartBEC = false;
+const enableSyncPlayerLoadouts = false;
+const enableSyncTrader = false;
+const enableSyncSubmodules = false;
+const enableSyncProfileByMap = true;
 
 const getPath = p => p ? path.resolve(__dirname, p) : __dirname;
 
@@ -24,34 +33,63 @@ switch (true) {
 
 console.log(`###### Building for map "${mapName}"`);
 
-// Pull updates from heros-haven-dayz-server repo
-execSync('git pull', { cwd: getPath() }, (err, stdout, stderr) => {
-    if (err || stderr) console.log(err || stderr);
-    console.log(stdout);
-    console.log('###### Server repo pulled');
-
-    // Sync submodules
-    execSync('git submodule update --init --recursive', { cwd: getPath() }, (err, stdout, stderr) => {
+// Sync with profile repo
+if (enableSyncSubmodules) {
+    execSync('git submodule update --init --recursive', { cwd: getPath('profiles'), shell: true, detached: true }, (err, stdout, stderr) => {
         if (err || stderr) console.log(err || stderr);
         console.log(stdout);
         console.log('###### Submodules synced');
-    })
-});
+    });
+}
 
-// Start BEC
-const BEC = spawn('BEC.exe', ['-f Config.cfg', '--dsc'], { cwd: getPath('BEC'), shell: true, detached: true });
-console.log('###### Starting BEC');
+// Pull updates from heros-haven-dayz-server repo
+if (enableSyncThis) {
+    execSync('git pull', { cwd: getPath() }, (err, stdout, stderr) => {
+        if (err || stderr) console.log(err || stderr);
+        console.log(stdout);
+        console.log('###### Server repo pulled');
 
-// Install PlayerLoadouts dependancies and build
-execSync('npm start', { cwd: getPath('profiles/PlayerLoadouts'), shell: true, detached: true }, (err, stdout, stderr) => {
-    if (err || stderr) console.log(err || stderr);
-    console.log(stdout);
-    console.log('###### PlayerLoadouts repo installed and built');
-});
+        // Sync submodules
+        execSync('git submodule update --init --recursive', { cwd: getPath() }, (err, stdout, stderr) => {
+            if (err || stderr) console.log(err || stderr);
+            console.log(stdout);
+            console.log('###### Submodules synced');
+        })
+    });
+}
+
+if (enableStartBEC) {
+    // Start BEC
+    const BEC = spawn('BEC.exe', ['-f Config.cfg', '--dsc'], { cwd: getPath('BEC'), shell: true, detached: true });
+    console.log('###### Starting BEC');
+}
+
+if (enableSyncPlayerLoadouts) {
+    // Install PlayerLoadouts dependancies and build
+    execSync('npm start', { cwd: getPath('profiles/PlayerLoadouts'), shell: true, detached: true }, (err, stdout, stderr) => {
+        if (err || stderr) console.log(err || stderr);
+        console.log(stdout);
+        console.log('###### PlayerLoadouts repo installed and built');
+    });
+}
 
 // Install Trader dependancies and build
-execSync(`npm install && npm run build-${mapName}`, { cwd: getPath('profiles/Trader'), shell: true, detached: true }, (err, stdout, stderr) => {
-    if (err || stderr) console.log(err || stderr);
-    console.log(stdout);
-    console.log('###### Trader repo installed and built');
-});
+if (enableSyncTrader) {
+    execSync(`npm install && npm run build-${mapName}`, { cwd: getPath('profiles/Trader'), shell: true, detached: true }, (err, stdout, stderr) => {
+        if (err || stderr) console.log(err || stderr);
+        console.log(stdout);
+        console.log('###### Trader repo installed and built');
+    });
+}
+
+if (enableSyncProfileByMap) {
+    // Copy profile configuration for map
+    if (getMap && fs.existsSync(getPath(`profiles/_MAPS/${getMap}`))) {
+        fse.copySync(getPath(`profiles/_MAPS/${getMap}/`), getPath('profiles/'), { overwrite: true }, err => {
+            if (err) console.error(err);
+            console.log("success!");
+        });
+    }
+}
+
+// { overwrite: true }
